d5c80a024143f9432bb4de531c01997ede4dde5f
iomc4u <iomc4u@gmail.com>
2021-03-21 22:47:24 +0900

    travis ci deploy added

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: "mysql",
-    user: "root",
-    password: "iomc4u",
-    database: "myapp"
+    // host: "mysql",
+    // user: "root",
+    // password: "iomc4u",
+    // database: "myapp"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 
 exports.pool = pool;
